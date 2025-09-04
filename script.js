f8fd91b49e19576487a8a6bec64f9c493390ef89
GabriellyDrosda <141978761+BloodyGaby@users.noreply.github.com>
2025-09-04 17:18:04 -0300

    .

---
+++ script.js
@@ -0,0 +1,54 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        
+        enunciado:
+        "1. Qual foi a primeira IA a ser utilizada? ",
+
+        alternativas:[
+            "Alexa",
+             "Logic Theorist"
+        ]
+    },
+    {
+        enunciado:
+        "2.Qual dos seguintes é um exemplo de aplicação de IA no dia a dia",
+
+        alternativas:[
+            "Apenas jogos de computador complexos.",
+             "Assistentes virtuais, como a Siri ou a Alexa, e sistemas de recomendação da Netflix"
+        ]
+    },
+    {
+        enunciado:
+        "3.Qual é o principal objetivo do aprendizado de máquina (machine learning)?",
+
+        alternativas:[
+            "Apenas criar bancos de dados e armazenar informações de forma organizada.",
+             "Fazer com que os computadores aprendam a partir de dados sem serem explicitamente programados para cada tarefa."
+        ]
+    },
+    {
+        enunciado:
+        "4. O que é IA generativa?",
+
+        alternativas:[v
+            "Um tipo de inteligência artificial que cria novos dados, como texto, imagens ou música." ,
+             "Um tipo de inteligência artificial que apenas analisa e classifica dados existentes."
+        ]
+    },
+    {
+        enunciado:
+        "5. O que se espera sobre os modelos de linguagem no futuro próximo?",
+
+        alternativas:[
+            "Serão mais eficientes, personalizados e com menor consumo de energia",
+             "Serão sempre maiores, mais lentos e inacessíveis ao público geral"
+        ]
+    },
+]
