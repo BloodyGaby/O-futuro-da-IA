f3040a8633a903378a07be1a9330f7fef234fb16
GabriellyDrosda <141978761+BloodyGaby@users.noreply.github.com>
2025-10-23 16:36:40 -0300

    .

--- script.js
+++ script.js
@@ -52,31 +52,31 @@ const perguntas = [
         ]
     },
 ]
-    let atual = 0;
-    let pergutaAtual;
-    let historiaFinal = "";
+  let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
-    function mostraPergunta(){
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
-        mostraAlternativas();
-    }
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
 
-    function mostraAlternativa(){
-        for (const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);    
-        }
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
-    function respostaSelecionada(opcaoSelecionada){
-        const afirmacoes = opcaoSelecionada.afirmacoes;
-        historiaFinal = afirmacoes;
-        atual++;
-        mostraPergunta();
-    }
+}
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
     mostraPergunta();
+}
 
+mostraPergunta();
 
