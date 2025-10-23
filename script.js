a54c6f9511c98155266a174d5f5ce0fdd4d64bb4
GabriellyDrosda <141978761+BloodyGaby@users.noreply.github.com>
2025-10-23 16:26:59 -0300

    .

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const perguntas = [
 ]
     let atual = 0;
     let pergutaAtual;
+    let historiaFinal = "";
 
     function mostraPergunta(){
         perguntaAtual = perguntas[atual];
@@ -64,9 +65,18 @@ const perguntas = [
     function mostraAlternativa(){
         for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textCOntent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);    
         }
     }
+    function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacoes;
+        historiaFinal = afirmacoes;
+        atual++;
+        mostraPergunta();
+    }
 
     mostraPergunta();
+
+
